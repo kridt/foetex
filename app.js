af8467573a1b82ff630a41b2523947a947ef5635
Christian <chrnielsen2003@gmail.com>
2021-01-06 00:27:36 +0100

    cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 var express = require("express");
 var app = express();
 var formidable = require("express-formidable");
+var cors = require("cors");
 
 require("./database");
+app.use(cors());
 app.use(formidable());
 
 require("./coworkers.route")(app);
